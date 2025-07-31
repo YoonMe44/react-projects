c2095cddf7cefd9ac92d12bbdbcd7f15362a29fd
Yoonme <yoonmem86@gmail.com>
2025-07-31 17:12:10 +0900

    The first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Navigation from "./components/Navigation";
+const App = () => {
+    return<div>
+        <Navigation/>
+    </div>
+};
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation =()=>{
+    return<div>
+        <nav>
+            <div className="logo">
+                <img src="../public/images/Apple-Logosu.png" alt="logo" />
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    </div>
+}
+export default Navigation;
